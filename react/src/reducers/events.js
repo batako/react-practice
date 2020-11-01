04474023a266d615063d2de5d6dc8e207a538ff8
batako <guri725ren213@gmail.com>
2020-11-01 12:22:59 +0900

    event削除から一覧に戻った際に削除内容が反映されるように

--- react/src/reducers/events.js
+++ react/src/reducers/events.js
@@ -1,5 +1,8 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions/events'
+import {
+  READ_EVENTS,
+  DELETE_EVENT,
+} from '../actions/events'
 
 export default (events = {}, action) => {
   switch (action.type) {
@@ -7,6 +10,10 @@ export default (events = {}, action) => {
       // console.log(action.response.data)
       // console.log(_.mapKeys(action.response.data, 'id'))
       return _.mapKeys(action.response.data, 'id')
+
+    case DELETE_EVENT:
+      delete events[action.id]
+      return { ...events }
 
     default:
       return events
